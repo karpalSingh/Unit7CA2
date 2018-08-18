34a8dd82d15a555c0768fb1e2095b4bea85d9241
Karpal Singh <karpal@live.com.sg>
2018-08-18 15:33:23 +0800

    PC version of batch files
    Created batch files that will be used to run in Windows.

---
+++ setenv.bat
@@ -0,0 +1,6 @@
+set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_181
+set ANT_HOME=C:\Users\Dev\Documents\apache-ant-1.10.5
+set JUNIT_HOME=C:\Users\Dev\Documents\junit3.8.2
+
+set PATH=%JAVA_HOME%\bin;%ANT_HOME%\bin
+set CLASSPATH=%JUNIT_HOME%\junit.jar
